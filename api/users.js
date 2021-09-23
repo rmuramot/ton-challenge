76a3d2666446af3abb0cc6a095a42ecfef0ae9ca
rmuramot <renanmuramoto@gmail.com>
2021-09-23 11:58:34 -0300

    feat: created api structure and submit user

--- api/users.js
+++ api/users.js
@@ -7,6 +7,68 @@ AWS.config.setPromisesDependency(require('bluebird'));
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.submit = (event, context, callback) => {};
+module.exports.submit = (event, context, callback) => {
+    const requestBody = JSON.parse(event.body);
+    const fullname = requestBody.fullname;
+    const email = requestBody.email;
+    const phone = requestBody.phone;
 
-module.exports.get = (event, context, callback) => {};
+    if (typeof fullname !== 'string' || typeof email !== 'string' || typeof phone !== 'number'){
+        console.error('Validation Failed');
+        callback(new Error('Couldn\'t submit user, check if the fields were correctly filled.'))
+        return;
+    }
+
+    submitUsers(userInfo(fullname, email, phone))
+        .then(res => {
+            callback(null, {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'User sucessfully registered'
+                })
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            callback(null,{
+                statusCode: 500,
+                body: JSON.stringify({
+                    message: 'Couldn\'t submit user.'
+                })
+            })
+        });
+};
+
+module.exports.get = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'user get endpoint',
+            input: event,
+        }),
+    };
+    
+    callback(null, response);
+};
+
+const submitUsers = user =>{
+    console.log('Submitting user');
+    const userInfo = {
+        TableName: process.env.USERS_TABLE,
+        Item: user,
+    };
+    return dynamoDb.put(userInfo).promise()
+        .then(res => user);
+};
+
+const userInfo = (fullname, email, phone) => {
+    const timestamp = new Date().getTime();
+    return {
+        id: uuidv4(),
+        fullname: fullname,
+        email: email,
+        phone: phone,
+        submittedAt: timestamp,
+        updatedAt: timestamp,
+    };
+};
--- api/views.js
+++ api/views.js
@@ -1,12 +1,33 @@
 'use strict';
 
 const { v4: uuidv4 } = require('uuid');
-const AWS = require('aws-sdk'); 
+const AWS = require('aws-sdk');
+const countapi = require('countapi-js');
 
 AWS.config.setPromisesDependency(require('bluebird'));
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.increment = (event, context, callback) => {};
+module.exports.increment = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'views increment endpoint',
+            input: event,
+        }),
+    };
+    
+    callback(null, response);
+};
 
-module.exports.list = (event, context, callback) => {};
+module.exports.list = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'views list endpoint',
+            input: event,
+        }),
+    };
+    
+    callback(null, response);
+};
